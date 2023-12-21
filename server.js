7eed6b80a258cbd9c773f8bc8c8bc995c0e2c023
bryant tunbutr <bryantt23@users.noreply.github.com>
2023-12-21 01:24:28 -0800

    get post from mongodb

--- server.js
+++ server.js
@@ -1,15 +1,23 @@
 const express = require('express');
 const app = express();
 const port = 3000;
+require('dotenv').config();
+const mongoose = require('mongoose');
+const Post = require('./models/post');
 
-// Dummy data for demonstration
-let posts = [
-  { id: 1, title: 'Hello World', content: 'This is my first post!' }
-  // ... more posts
-];
+const mongoDb = process.env.MONGODB_URI;
+mongoose.connect(mongoDb);
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'mongo connection error'));
+const postsCollection = db.collection('posts');
 
-app.get('/posts', (req, res) => {
-  res.json(posts);
+app.get('/posts', async (req, res) => {
+  try {
+    const posts = await Post.find();
+    res.json(posts);
+  } catch (error) {
+    res.status(500).send(error.message);
+  }
 });
 
 app.listen(port, () => {
