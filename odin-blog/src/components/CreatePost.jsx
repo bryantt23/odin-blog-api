2b0d0f8ee52467be819e475d042dd5d79f3f1f1d
bryant tunbutr <bryantt23@users.noreply.github.com>
2023-12-22 03:41:22 -0800

    can create new post

--- odin-blog/src/components/CreatePost.jsx
+++ odin-blog/src/components/CreatePost.jsx
@@ -1,6 +1,46 @@
+import React, { useState } from 'react';
+import axios from 'axios';
+import { useNavigate } from 'react-router-dom';
+
 function CreatePost() {
-    // Logic to fetch and display post
-    return <div>CreatePost will be shown here.</div>;
+    const [title, setTitle] = useState('');
+    const [body, setBody] = useState('');
+    const navigate = useNavigate();
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        const token = localStorage.getItem('jwt');
+
+        try {
+            const response = await axios.post('http://localhost:3000/posts',
+                { title, body },
+                { headers: { Authorization: `Bearer ${token}` } }
+            );
+
+            if (response.status === 201) {
+                navigate('/posts');
+            }
+        } catch (error) {
+            console.error('Error creating post:', error);
+        }
+    };
+
+    return (
+        <form onSubmit={handleSubmit}>
+            <input
+                type="text"
+                placeholder="Title"
+                value={title}
+                onChange={(e) => setTitle(e.target.value)}
+            />
+            <textarea
+                placeholder="Body"
+                value={body}
+                onChange={(e) => setBody(e.target.value)}
+            />
+            <button type="submit">Create Post</button>
+        </form>
+    );
 }
 
 export default CreatePost;
