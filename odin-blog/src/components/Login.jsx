371697070fd097ceb3004c8095107bdcdfb53217
bryant tunbutr <bryantt23@users.noreply.github.com>
2023-12-22 03:30:50 -0800

    login & redirect

--- odin-blog/src/components/Login.jsx
+++ odin-blog/src/components/Login.jsx
@@ -1,10 +1,45 @@
+import React, { useState } from 'react';
+import axios from 'axios';
+import { useNavigate } from 'react-router-dom';
+
 function Login() {
-    const handleLogin = () => {
-        // Perform login logic, then set login status
-        localStorage.setItem("isLoggedIn", "true");
+    const [username, setUsername] = useState('');
+    const [password, setPassword] = useState('');
+    const navigate = useNavigate();
+
+    const handleLogin = async (e) => {
+        e.preventDefault();
+
+        try {
+            const response = await axios.post('http://localhost:3000/login', { username, password });
+            localStorage.setItem("jwt", response.data.token);
+            navigate('/create-post');
+        } catch (error) {
+            if (error.response && error.response.status === 401) {
+                alert('Authentication failed');
+            } else {
+                console.error('Login error:', error);
+            }
+        }
     };
 
-    return <button onClick={handleLogin}>Login</button>;
+    return (
+        <form onSubmit={handleLogin}>
+            <input
+                type="text"
+                placeholder="Username"
+                value={username}
+                onChange={(e) => setUsername(e.target.value)}
+            />
+            <input
+                type="password"
+                placeholder="Password"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+            />
+            <button type="submit">Login</button>
+        </form>
+    );
 }
 
 export default Login;
--- odin-blog/src/components/ProtectedRoute.jsx
+++ odin-blog/src/components/ProtectedRoute.jsx
@@ -1,9 +1,9 @@
 import { Navigate } from 'react-router-dom'
 
 const ProtectedRoute = ({ children }) => {
-    const isLoggedIn = localStorage.getItem("isLoggedIn")
+    const hasJwt = localStorage.getItem("jwt")
 
-    if (!isLoggedIn) {
+    if (!hasJwt) {
         return <Navigate to="/login" />
     }
 
